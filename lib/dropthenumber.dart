785b7445a9ded3e92afaab4891ee5b409bb0e715
Cangyu Lin <jefflin.je598@gmail.com>
2021-06-28 06:21:41 +0800

    add comments

--- lib/dropthenumber.dart
+++ lib/dropthenumber.dart
@@ -5,10 +5,12 @@ class DropTheNumber extends Game {
   Size screenSize = Size(500, 750);
 
   void render(Canvas canvas) {
+    // draw background
     Rect bgRect = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
     Paint bgPaint = Paint();
     bgPaint.color = Color(0xff000000);
     canvas.drawRect(bgRect, bgPaint);
+    // draw box
     double screenCenterX = screenSize.width / 2;
     double screenCenterY = screenSize.height / 2;
     Rect boxRect =
