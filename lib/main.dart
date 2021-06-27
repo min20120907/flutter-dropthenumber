04936213907e808806fabaff43ff060c635e24c5
Cangyu Lin <jefflin.je598@gmail.com>
2021-06-28 01:10:43 +0800

    initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
+import 'package:flame/util.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:dropthenumber/dropthenumber.dart';
 
-void main() {
+void main() async {
+  Util flameUtil = Util();
+  await flameUtil.fullScreen();
+  await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+
+  DropTheNumber game = DropTheNumber();
+  runApp(game.widget);
 }
-
