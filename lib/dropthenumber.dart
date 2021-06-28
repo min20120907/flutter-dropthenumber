c136d2304bda954d67e8b85b962855305de98258
Cangyu Lin <jefflin.je598@gmail.com>
2021-06-29 00:15:54 +0800

    fix back ground pic

--- lib/dropthenumber.dart
+++ lib/dropthenumber.dart
@@ -1,28 +1,23 @@
 // @dart=2.11
+
 import 'dart:async';
 import 'dart:typed_data';
-import 'dart:ui' as UI;
+import 'dart:ui' as ui;
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart' as widgets;
+import 'package:flutter/widgets.dart';
 
 class DropTheNumber extends Game {
   Size screenSize;
-  UI.Image bgImage;
+  ui.Image img;
   void render(Canvas canvas) {
     // draw background
-    Rect bgRect = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
-    Paint bgPaint = Paint();
-    bgPaint.color = Color(0xff000000);
-    canvas.drawRect(bgRect, bgPaint);
+    drawImage(Paint(), canvas, "/img/bg3.jpg", 0, 0, screenSize.width,
+        screenSize.height);
 
-    // pygame.draw.rect(screen, white, (50,25,400,650), 5)
-    Rect Rect1 = Rect.fromLTWH(
-      screenSize.width / 10, 
-      screenSize.height / 30,
-      screenSize.width * 4 / 5,
-      screenSize.height * 650 / 750);
+    Rect Rect1 = Rect.fromLTWH(screenSize.width / 10, screenSize.height / 30,
+        screenSize.width * 4 / 5, screenSize.height * 650 / 750);
 
     Paint rect1Paint = Paint()
       ..color = Color(0xffffffff)
@@ -42,28 +37,15 @@ class DropTheNumber extends Game {
       ..style = PaintingStyle.stroke
       ..strokeWidth = 10;
     canvas.drawRect(Rect2, rect2Paint);
-
     //draw three horizontal lines
     drawLine(Colors.white, canvas, 50, 75, 450, 75, 5);
     drawLine(Colors.white, canvas, 50, 125, 450, 125, 5);
     drawLine(Colors.white, canvas, 75, 220, 425, 220, 5);
-
     // draw five vertical lines
     for (double i = 0; i < 5; i++)
       drawLine(Colors.white, canvas, 75 + i * 70, 150, 75 + i * 70, 650, 5);
 
-    drawImage(Paint(), canvas, "/img/bg3.jpg", 0, 0, screenSize.width,
-        screenSize.height);
     drawText(canvas, 'Hello world!', 10, 10);
-  }
-
-  Future<UI.Image> loadUiImage(String imageAssetPath) async {
-    final ByteData data = await rootBundle.load(imageAssetPath);
-    final Completer<UI.Image> completer = Completer();
-    UI.decodeImageFromList(Uint8List.view(data.buffer), (UI.Image img) {
-      return completer.complete(img);
-    });
-    return completer.future;
   }
 
   void drawLine(Color c, Canvas canvas, double p1x, double p1y, double p2x,
@@ -89,14 +71,29 @@ class DropTheNumber extends Game {
           Offset(screenSize.width * x / 500, screenSize.height * y / 750));
   }
 
+  Future<ui.Image> loadUiImage(String imageAssetPath) async {
+    final ByteData data = await rootBundle.load(imageAssetPath);
+    final Completer<ui.Image> completer = Completer();
+    ui.decodeImageFromList(Uint8List.view(data.buffer), (ui.Image img) {
+      return completer.complete(img);
+    });
+    return completer.future;
+  }
+
   void drawImage(Paint p, Canvas canvas, String imgPath, double x, double y,
       double sx, double sy) {
-    widgets.Image img = widgets.Image.asset(imgPath);
-    paintImage(
-        canvas: canvas,
-        rect: Rect.fromLTWH(
-            screenSize.width * x / 500, screenSize.height * y / 750, sx, sy),
-        image: img);
+    // Image img = Image(image: AssetImage('img/bg3.jpg'));
+
+    loadUiImage("img/bg3.jpg").then((value) {
+      this.img = value;
+    }).whenComplete(() => print("done"));
+
+    canvas.drawImageRect(
+        this.img,
+        Rect.fromLTWH(screenSize.width * x / 500, screenSize.height * y / 750,
+            img.width.toDouble(), img.height.toDouble()),
+        Rect.fromLTWH(screenSize.width * x / 500, y, sx, sy),
+        p);
   }
 
   void update(double t) {}
