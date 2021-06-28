a342dfca87182f9df8c32d3539d4e0bb3ece097a
Allen-wa-ng <wang0911309998@gmail.com>
2021-06-28 15:39:57 +0800

    asa

--- lib/dropthenumber.dart
+++ lib/dropthenumber.dart
@@ -4,34 +4,30 @@ import 'package:flame/game.dart';
 
 class DropTheNumber extends Game {
   Size screenSize = Size(500, 750);
-
+  
   void render(Canvas canvas) {
     // draw background
     Rect bgRect = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
     Paint bgPaint = Paint();
     bgPaint.color = Color(0xff000000);
     canvas.drawRect(bgRect, bgPaint);
-<<<<<<< HEAD
-    // draw box
-    double screenCenterX = screenSize.width / 2;
-    double screenCenterY = screenSize.height / 2;
-    Rect boxRect =
-        Rect.fromLTWH(screenCenterX - 75, screenCenterY - 75, 150, 150);
-    Paint boxPaint = Paint();
-    boxPaint.color = Color(0xffffffff);
-    canvas.drawRect(boxRect, boxPaint);
-=======
     // pygame.draw.rect(screen, white, (50,25,400,650), 5)
     Rect Rect1 = Rect.fromLTWH(screenSize.width / 10, screenSize.height / 30,
         screenSize.width * 4 / 5, screenSize.height * 650 / 750);
-    Paint rect1Paint = Paint();
-    rect1Paint.color = Color(0xffffffff);
+    Paint rect1Paint = Paint()
+    ..strokeWidth = 4;
     canvas.drawRect(Rect1, rect1Paint);
-    // pygame.draw.lines(screen, white, True,[(50,75), (450,75)],5)
+    //pygame.draw.rect(screen, white, (75,150,350,500), 5)
+    Rect Rect2 = Rect.fromLTWH(screenSize.width* 75/ 500, screenSize.height *15/ 75,
+        screenSize.width * 35/ 50, screenSize.height * 50 / 75);
+    Paint rect2Paint = Paint();
+    rect2Paint.color = Color(0000000000);
+    canvas.drawRect(Rect2, rect2Paint);
 
+    // pygame.draw.lines(screen, white, True,[(50,75), (450,75)],5)
+    
     // pygame.draw.lines(screen, white, True,[(50,125),(450,125)],5)
     // pygame.draw.lines(screen, white, True,[(75,220),(425,220)],5)
->>>>>>> 8cf70f367398f9e6a8c7db777accf710d46e336f
   }
 
   void update(double t) {}
