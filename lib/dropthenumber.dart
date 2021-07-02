b3166d9fc4bf30e574195c9cb958b4337bf7f9b8
Allen-wa-ng <wang0911309998@gmail.com>
2021-07-02 17:37:47 +0800

    as

--- lib/dropthenumber.dart
+++ lib/dropthenumber.dart
@@ -75,7 +75,7 @@ class DropTheNumber extends Game with TapDetector {
       }
       // draw superpower horizontal
       loadUiImage("img/fire-4.png").then((value) => img3 = value);
-      drawImage(Paint(), canvas, img3, 402, 685, getX(59), getY(60));
+      drawImage(Paint(), canvas, img3, 403, 685, getX(59), getY(60));
       // draw superpower vertical
       loadUiImage("img/vertical-2.png").then((value) => img4 = value);
       drawImage(Paint(), canvas, img4, 350, 686, getX(50), getY(50));
