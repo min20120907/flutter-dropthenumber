7d20b10c2302198d971aaa8b05f8a651a7d9e1f3
Cangyu Lin <jefflin.je598@gmail.com>
2021-08-22 10:19:39 +0800

    fix start button

--- lib/dropthenumber.dart
+++ lib/dropthenumber.dart
@@ -656,7 +656,7 @@ class DropTheNumber extends Game with TapDetector {
 
     // Game start
     if (!startGameScreenFinished) {
-      if (inRange(x, 60, 98) && inRange(y, 20, 26)) {
+      if (inRange(x, 21, 80) && inRange(y, 30, 40)) {
         startGameScreenFinished = true;
       }
       //   if (inRange(x, 20, 30) && inRange(y, 30, 40)) {
