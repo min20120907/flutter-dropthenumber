71fa158bdd7ba832dcb30d5c1d2da318a5c83398
cpprust <cpprust@gmail.com>
2024-02-24 10:37:49 +0800

    Remove force orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,11 @@
-import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'dropthenumber.dart';
 import 'data_handler.dart';
 
 void main() async {
-  Flame.device.fullScreen();
-  Flame.device.setOrientation(DeviceOrientation.portraitUp);
-  Flame.device.setPortraitUpOnly();
-
   // SharedPreferences "Null check operator used on a null value" problem
   // Solution found: https://stackoverflow.com/a/62493934
   WidgetsFlutterBinding.ensureInitialized();
